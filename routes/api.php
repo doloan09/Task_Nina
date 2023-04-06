e385694f6b6505e1e37dd135a3e3255a10888efc
Do Thi Loan <dothiloan150901@gmail.com>
2023-04-06 21:22:00 +0700

    add routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\ClassHPController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PointController;
+use App\Http\Controllers\SemesterController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,15 +28,60 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // login
 Route::post('/login', [AuthController::class, 'login'])->name('login'); // dang nhap
-Route::post('users/create', [UserController::class, 'store'])->name('users.create'); // dang ky
-Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout'); // dang xuat
-
-// user // , 'admin'
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index'); // danh sach user
-});
+Route::post('/register', [UserController::class, 'store'])->name('register'); // dang ky
+Route::get('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum'])->name('logout'); // dang xuat
 
 Route::post('/forgot-password', [ResetPasswordController::class, 'forgotPass'])->middleware('guest')->name('password.email'); // gui mail kem link de reset pass
 Route::get('/reset-password/{token}', [ResetPasswordController::class, 'tokenReset'])->middleware('guest')->name('password.reset');
 Route::post('/reset-password', [ResetPasswordController::class, 'resetPass'])->middleware('guest')->name('password.update'); // reset pass
 
+
+Route::prefix('v1')
+    ->name('v1.')
+    ->group(function () {
+        Route::name('users.')
+            ->prefix('users')
+            ->group(function () {
+                Route::get('/', [UserController::class, 'index'])->name('index'); // danh sach user
+                Route::post('create', [UserController::class, 'create'])->name('create');
+                Route::post('/store', [UserController::class, 'store'])->name('store'); // dang ky
+                Route::post('update/{id}', [UserController::class, 'update'])->name('update');
+                Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
+            });
+
+        Route::name('subjects.')
+            ->prefix('subjects')
+            ->group(function () {
+                Route::get('/', [SubjectController::class, 'index'])->name('index'); // danh sach mon hoc
+                Route::post('create', [SubjectController::class, 'create'])->name('create');
+                Route::post('update', [SubjectController::class, 'update'])->name('update');
+            });
+
+        Route::name('semesters.')
+            ->prefix('semesters')
+            ->group(function (){
+                Route::get('/', [SemesterController::class, 'index']);
+                Route::get('/{id}', [SemesterController::class, 'show']);
+            });
+
+        Route::name('notifications.')
+            ->prefix('notifications')
+            ->group(function (){
+                Route::get('/', [NotificationController::class, 'index']);
+                Route::get('/{id}', [NotificationController::class, 'show']);
+                Route::post('/', [NotificationController::class, 'store']);
+            });
+
+        Route::name('classes.')
+            ->prefix('classes')
+            ->group(function (){
+                Route::get('/', [ClassHPController::class, 'index']);
+            });
+
+        Route::name('points.')
+            ->prefix('points')
+            ->group(function (){
+                Route::get('/', [PointController::class, 'index']);
+            });
+
+    });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\ClassHPController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PointController;
+use App\Http\Controllers\SemesterController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +24,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'viewLogin'])->name('login.request');
+Route::get('/register', [AuthController::class, 'viewRegister'])->name('register.request');
+
+/// forgot password
+Route::get('/forgot-password', [ResetPasswordController::class, 'showForgotPass'])->middleware('guest')->name('password.request'); // view forgot pass
+
+//Route::middleware('auth:sanctum')->group(function (){
+    Route::prefix('users')
+        ->group(function (){
+            Route::get('/', [UserController::class, 'list'])->name('users.list'); // view danh sach user
+            Route::get('/create', [UserController::class, 'create'])->name('users.create'); // view create user
+            Route::get('/{id}/edit', [UserController::class, 'edit'])->name('users.edit'); // view edit user
+
+        });
+
+    Route::prefix('notifications')
+        ->group(function (){
+            Route::get('/', [NotificationController::class, 'list'])->name('notifications.list'); // view danh sach notifications
+            Route::get('/create', [NotificationController::class, 'create'])->name('notifications.create'); // view create notifications
+            Route::get('/{id}/edit', [NotificationController::class, 'edit'])->name('notifications.edit'); // view edit notifications
+
+        });
+
+    Route::prefix('semesters')
+        ->group(function (){
+            Route::get('/', [SemesterController::class, 'list'])->name('semesters.list'); // view danh sach user
+            Route::get('/create', [SemesterController::class, 'create'])->name('semesters.create'); // view create user
+            Route::get('/{id}/edit', [SemesterController::class, 'viewUpdate'])->name('semesters.edit'); // view edit user
+
+        });
+
+    Route::prefix('subjects')
+        ->group(function (){
+            Route::get('/', [SubjectController::class, 'list'])->name('subjects.list'); // view danh sach user
+            Route::get('/create', [SubjectController::class, 'create'])->name('subjects.create'); // view create user
+            Route::get('/{id}/edit', [SubjectController::class, 'edit'])->name('subjects.edit'); // view edit user
+
+        });
+
+    Route::prefix('classes')
+        ->group(function (){
+            Route::get('/', [ClassHPController::class, 'list'])->name('classes.list'); // view danh sach user
+            Route::get('/create', [ClassHPController::class, 'create'])->name('classes.create'); // view create user
+            Route::get('/{id}/edit', [ClassHPController::class, 'edit'])->name('classes.edit'); // view edit user
+
+        });
+
+    Route::prefix('points')
+        ->group(function (){
+            Route::get('/', [PointController::class, 'list'])->name('points.list'); // view danh sach user
+            Route::get('/create', [PointController::class, 'create'])->name('points.create'); // view create user
+            Route::get('/{id}/edit', [PointController::class, 'edit'])->name('points.edit'); // view edit user
+
+        });
+
+//});
