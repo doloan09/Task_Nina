3af8c6c81aed3c3e0712fac4a4dadf8b940257d2
Do Thi Loan <dothiloan150901@gmail.com>
2023-04-08 00:08:56 +0700

    update seeder

--- database/seeders/PermissionsDemoSeeder.php
+++ database/seeders/PermissionsDemoSeeder.php
@@ -23,27 +23,35 @@ class PermissionsDemoSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
+        Permission::create(['name' => 'users']);
+        Permission::create(['name' => 'subjects']);
+        Permission::create(['name' => 'semesters']);
+        Permission::create(['name' => 'notifications']);
+        Permission::create(['name' => 'points']);
+        Permission::create(['name' => 'classes']);
 
         // create roles and assign existing permissions
-        $role1 = Role::create(['name' => 'writer']);
-        $role1->givePermissionTo('edit articles');
-        $role1->givePermissionTo('delete articles');
+        $role1 = Role::create(['name' => 'student']);
 
         $role2 = Role::create(['name' => 'admin']);
-        $role2->givePermissionTo('publish articles');
-        $role2->givePermissionTo('unpublish articles');
+        $role2->givePermissionTo('users');
+        $role2->givePermissionTo('subjects');
+        $role2->givePermissionTo('semesters');
+        $role2->givePermissionTo('notifications');
+        $role2->givePermissionTo('points');
+        $role2->givePermissionTo('classes');
 
-        $role3 = Role::create(['name' => 'Super-Admin']);
+        $role3 = Role::create(['name' => 'teacher']);
+        $role3->givePermissionTo('users');
+        $role3->givePermissionTo('points');
+        $role3->givePermissionTo('classes');
+
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
         // create demo users
         $user = User::query()->create([
-            'name'          => 'Example User',
-            'email'         => 'test@example.com',
+            'name'          => 'Student',
+            'email'         => 'student@example.com',
             'password'      => Hash::make('12345678'),
             'code_user'     => '191204220',
             'date_of_birth' => '2001-09-14',
@@ -51,7 +59,7 @@ class PermissionsDemoSeeder extends Seeder
         $user->assignRole($role1);
 
         $user = User::query()->create([
-            'name'          => 'Example Admin User',
+            'name'          => 'Admin',
             'email'         => 'admin@example.com',
             'password'      => Hash::make('12345678'),
             'code_user'     => '191204221',
@@ -60,8 +68,8 @@ class PermissionsDemoSeeder extends Seeder
         $user->assignRole($role2);
 
         $user = User::query()->create([
-            'name'          => 'Example Super-Admin User',
-            'email'         => 'superadmin@example.com',
+            'name'          => 'Teacher',
+            'email'         => 'teacher@example.com',
             'password'      => Hash::make('12345678'),
             'code_user'     => '191204222',
             'date_of_birth' => '2001-09-16',
