91fa46ae7053c2884022d6fd76947953e1545519
Do Thi Loan <dothiloan150901@gmail.com>
2023-04-09 21:42:21 +0700

    module subject

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubjectRequest;
 use App\Http\Resources\FailedCollection;
+use App\Http\Resources\SuccessCollection;
 use App\Repositories\Subject\SubjectRepositoryInterface;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
@@ -26,9 +28,14 @@ class SubjectController extends Controller
         try {
             $list = $this->subjectRepo->getAll();
 
-            return DataTables::of($list)->make(true);
+            return Datatables::of($list)
+                ->editColumn('action', function ($item) {
+                    return '<button class="btn btn-xs btn-warning" data-toggle="modal" data-target="#exampleModal" style="margin: 0px 10px;">Update</button><button onclick="deleteSub('. $item->id .')" class="btn btn-xs btn-danger btn-delete">Delete</button>';
+                })
+                ->rawColumns(['action'])
+                ->make(true);
         }catch (\Exception $e){
-            return new FailedCollection($e);
+            return new FailedCollection(collect([$e]));
         }
     }
 
@@ -46,11 +53,19 @@ class SubjectController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SubjectRequest $request)
     {
-        //
+        try {
+            $data = request(['name_subject', 'code_subject', 'number_of_credits']);
+
+            $item = $this->subjectRepo->create($data);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
@@ -80,22 +95,36 @@ class SubjectController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $item = $this->subjectRepo->update($id, $data);
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        try {
+            $item = $this->subjectRepo->find($id);
+            $this->subjectRepo->delete($id);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -4,7 +4,9 @@
     <div>
         <h3>Quản lý môn học</h3>
         <div style="margin-top: 20px; margin-bottom: 20px;">
-            <a href="#" class="btn btn-xs btn-warning">Create</a>
+            <button type="button" class="btn btn-xs btn-warning" data-toggle="modal" data-target="#createSubject">
+                Create
+            </button>
         </div>
         <table class="table table-bordered" id="users-table">
             <thead>
@@ -13,13 +15,60 @@
                 <th>Tên môn học</th>
                 <th>Mã môn học</th>
                 <th>Số tín chỉ</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                <th>Action</th>
             </tr>
             </thead>
         </table>
     </div>
-@stop
+
+    <!-- Modal create -->
+    <div class="modal fade" id="createSubject" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="display: flex; justify-content: center">
+                    <h3 class="modal-title" id="exampleModalLabel">Thông tin môn học</h3>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('v1.subjects.store') }}" id="create-subject" enctype="multipart/form-data" style="margin: 0px 20px;">
+                        @csrf
+                        <div class="row">
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Tên môn học</label>
+                                    <input id="name_subject" name="name_subject" class="form-control" type="text" placeholder="Nhập vào tên môn học ..." required>
+                                    <div style="margin-top: 5px; " id="div_err_name_subject">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Mã môn học</label>
+                                    <input id="code_subject" name="code_subject" class="form-control" type="text" placeholder="Nhập vào mã môn học ... " required>
+                                    <div style="margin-top: 5px; " id="div_err_code_subject">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Số tín chỉ</label>
+                                    <input id="number_of_credits" name="number_of_credits" class="form-control" type="text" placeholder="Nhập vào số tín chỉ ... " required>
+                                    <div style="margin-top: 5px; " id="div_err_number_of_credits">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="margin-top: 20px; margin-bottom: 20px; display: flex; justify-content: right; font-size: small;">
+                            <button type="submit" class="btn btn-xs btn-success" style="padding: 8px;">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
 
 @push('scripts')
     <script>
@@ -38,10 +87,73 @@
                     {data: 'name_subject', name: 'name_subject'},
                     {data: 'code_subject', name: 'code_subject'},
                     {data: 'number_of_credits', name: 'number_of_credits'},
-                    {data: 'created_at', name: 'created_at'},
-                    {data: 'updated_at', name: 'updated_at'}
+                    {data: 'action', name: ''},
                 ]
             });
         });
+
+        $("#create-subject").submit(function (e) {
+            e.preventDefault();
+
+            var formData = new FormData();
+
+            formData.append('name_subject', $("#name_subject").val());
+            formData.append('code_subject', $("#code_subject").val());
+            formData.append('number_of_credits', $("#number_of_credits").val());
+
+            $.ajax({
+                url: '{{ route('v1.subjects.store') }}',
+                processData: false,
+                contentType: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('input[name="_token"]').val(),
+                    "Authorization": "Bearer " + localStorage.getItem("token"),
+                },
+                method: "POST",
+                data: formData,
+                success: function (data) {
+                    if (data.response.code === 200) {
+                        toastr.success('Thêm mới môn học thành công!', 'Success');
+                        window.location = "{{ route('subjects.list') }}";
+                    }
+                },
+                error: function (err) {
+                    if (err.status === 422) {
+                        let errList = err.responseJSON.Err_Message;
+
+                        for (let key in errList) {
+                            $("#div_err_" + key).html(`<p style="color: red; font-size: small;">* ` + errList[key] + `</p>`);
+                        }
+
+                        console.log(err);
+                    }else if (err.status === 500){
+                        toastr.error(err.statusText);
+                        console.log(err);
+                    }
+                },
+            });
+            return true;
+        });
+
+        function deleteSub(id){
+            if (confirm('Ban co muon xoa khong?') === true) {
+                $.ajax({
+                    url: `http://nina-soft.com/api/v1/subjects/` + id,
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ @csrf_token() }}',
+                        "Authorization": "Bearer " + localStorage.getItem("token"),
+                    },
+                    method: "DELETE",
+                    success: function (data) {
+                        window.location = "{{ route('subjects.list') }}";
+                    },
+                    error: function (err) {
+                        alert('error');
+                        console.log(err);
+                    }
+                });
+            }
+        }
+
     </script>
 @endpush
