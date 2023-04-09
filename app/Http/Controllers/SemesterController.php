44495c1466b8777ae662676fbf8959e14104ab35
Do Thi Loan <dothiloan150901@gmail.com>
2023-04-09 21:43:23 +0700

    module semester

--- app/Http/Controllers/SemesterController.php
+++ app/Http/Controllers/SemesterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SemesterRequest;
 use App\Http\Resources\FailedCollection;
+use App\Http\Resources\SuccessCollection;
 use App\Repositories\Semester\SemesterRepositoryInterface;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
@@ -26,9 +28,14 @@ class SemesterController extends Controller
         try {
             $list = $this->semesterRepo->getAll();
 
-            return DataTables::of($list)->make(true);
+            return Datatables::of($list)
+                ->editColumn('action', function ($item) {
+                    return '<button class="btn btn-xs btn-warning" data-toggle="modal" data-target="#exampleModal" style="margin: 0px 10px;">Update</button><button onclick="deleteSemester('. $item->id .')" class="btn btn-xs btn-danger btn-delete">Delete</button>';
+                })
+                ->rawColumns(['action'])
+                ->make(true);
         }catch (\Exception $e){
-            return new FailedCollection($e);
+            return new FailedCollection(collect([$e]));
         }
     }
 
@@ -39,18 +46,26 @@ class SemesterController extends Controller
      */
     public function create()
     {
-        //
+        return view('semesters.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SemesterRequest $request)
     {
-        //
+        try {
+            $data = request(['name_semester', 'year_semester']);
+
+            $item = $this->semesterRepo->create($data);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
@@ -80,22 +95,36 @@ class SemesterController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $user = $this->semesterRepo->update($id, $data);
+            return new SuccessCollection($user);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        try {
+            $item = $this->semesterRepo->find($id);
+            $this->semesterRepo->delete($id);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
 
--- resources/views/semesters/list.blade.php
+++ resources/views/semesters/list.blade.php
@@ -4,7 +4,9 @@
     <div>
         <h3>Quản lý học kỳ</h3>
         <div style="margin-top: 20px; margin-bottom: 20px;">
-            <a href="#" class="btn btn-xs btn-warning">Create</a>
+            <button type="button" class="btn btn-xs btn-warning" data-toggle="modal" data-target="#createSemester">
+                Create
+            </button>
         </div>
         <table class="table table-bordered" id="semesters-table">
             <thead>
@@ -12,13 +14,52 @@
                 <th>Id</th>
                 <th>Kỳ</th>
                 <th>Năm học</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                <th style="width: 15%;">Action</th>
             </tr>
             </thead>
         </table>
     </div>
-@stop
+
+    <!-- Modal create -->
+    <div class="modal fade" id="createSemester" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="display: flex; justify-content: center">
+                    <h3 class="modal-title" id="exampleModalLabel">Thông tin học kỳ</h3>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('v1.semesters.store') }}" id="create-semester" enctype="multipart/form-data" style="margin: 0px 20px;">
+                        @csrf
+                        <div class="row">
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Kỳ</label>
+                                    <input id="name_semester" name="name_semester" class="form-control" type="text" placeholder="Nhập vào tên kỳ học ..." required>
+                                    <div style="margin-top: 5px; " id="div_err_name_semester">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Năm học</label>
+                                    <input id="year_semester" name="year_semester" class="form-control" type="text" placeholder="Nhập vào năm học ... " required>
+                                    <div style="margin-top: 5px; " id="div_err_year_semester">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="margin-top: 20px; margin-bottom: 20px; display: flex; justify-content: right; font-size: small;">
+                            <button type="submit" class="btn btn-xs btn-success" style="padding: 8px;">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
 
 @push('scripts')
     <script>
@@ -36,10 +77,79 @@
                     { data: 'id', name: 'id' },
                     { data: 'name_semester', name: 'name_semester' },
                     { data: 'year_semester', name: 'year_semester' },
-                    { data: 'created_at', name: 'created_at' },
-                    { data: 'updated_at', name: 'updated_at' }
+                    { data: 'action', name: '' },
+
+                    // {
+                    //     data: 'action',
+                    //     render: function (id) {
+                    //         return '<a href="#" class="btn btn-xs btn-warning" style="margin: 0px 10px;">Update</a><button class="btn btn-xs btn-danger btn-delete">Delete</button>';
+                    //     }
+                    // },
                 ]
             });
         });
+
+        $("#create-semester").submit(function (e) {
+            e.preventDefault();
+
+            var formData = new FormData();
+
+            formData.append('name_semester', $("#name_semester").val());
+            formData.append('year_semester', $("#year_semester").val());
+
+            $.ajax({
+                url: '{{ route('v1.semesters.store') }}',
+                processData: false,
+                contentType: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('input[name="_token"]').val(),
+                    "Authorization": "Bearer " + localStorage.getItem("token"),
+                },
+                method: "POST",
+                data: formData,
+                success: function (data) {
+                    if (data.response.code === 200) {
+                        toastr.success('Thêm mới học kỳ thành công!', 'Success');
+                        window.location = "{{ route('semesters.list') }}";
+                    }
+                },
+                error: function (err) {
+                    if (err.status === 422) {
+                        let errList = err.responseJSON.Err_Message;
+
+                        for (let key in errList) {
+                            $("#div_err_" + key).html(`<p style="color: red; font-size: small;">* ` + errList[key] + `</p>`);
+                        }
+
+                        console.log(err);
+                    }else if (err.status === 500){
+                        toastr.error(err.statusText);
+                        console.log(err);
+                    }
+                },
+            });
+            return true;
+        });
+
+        function deleteSemester(id){
+            if (confirm('Ban co muon xoa khong?') === true) {
+                $.ajax({
+                    url: `http://nina-soft.com/api/v1/semesters/` + id,
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ @csrf_token() }}',
+                        "Authorization": "Bearer " + localStorage.getItem("token"),
+                    },
+                    method: "DELETE",
+                    success: function (data) {
+                        window.location = "{{ route('semesters.list') }}";
+                    },
+                    error: function (err) {
+                        alert('error');
+                        console.log(err);
+                    }
+                });
+            }
+        }
+
     </script>
 @endpush
