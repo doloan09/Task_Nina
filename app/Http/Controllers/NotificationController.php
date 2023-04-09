92310536a5d5707ebdf7ee255d25da8d89633b98
Do Thi Loan <dothiloan150901@gmail.com>
2023-04-09 21:44:27 +0700

    module notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NotificationRequest;
 use App\Http\Resources\FailedCollection;
+use App\Http\Resources\SuccessCollection;
 use App\Repositories\Notification\NotificationRepositoryInterface;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
@@ -26,9 +28,17 @@ class NotificationController extends Controller
         try {
             $list = $this->notiRepo->getAll();
 
-            return DataTables::of($list)->make(true);
+            return Datatables::of($list)
+                ->editColumn('content', function ($item) {
+                    return '<p style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2; overflow: hidden; width: 500px;">' . $item->content . '</p>';
+                })
+                ->editColumn('action', function ($item) {
+                    return '<button class="btn btn-xs btn-warning" data-toggle="modal" data-target="#exampleModal" style="margin: 0px 10px;">Update</button><button onclick="deleteNoti('. $item->id .')" class="btn btn-xs btn-danger btn-delete">Delete</button>';
+                })
+                ->rawColumns(['content', 'action'])
+                ->make(true);
         }catch (\Exception $e){
-            return new FailedCollection($e);
+            return new FailedCollection(collect([$e]));
         }
     }
 
@@ -46,11 +56,19 @@ class NotificationController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(NotificationRequest $request)
     {
-        //
+        try {
+            $data = request(['title', 'content']);
+
+            $item = $this->notiRepo->create($data);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
@@ -80,22 +98,36 @@ class NotificationController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $user = $this->notiRepo->update($id, $data);
+            return new SuccessCollection($user);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return FailedCollection|SuccessCollection|\Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        try {
+            $item = $this->notiRepo->find($id);
+            $this->notiRepo->delete($id);
+
+            return new SuccessCollection($item);
+        }catch (\Exception $e){
+            return new FailedCollection(collect([$e]));
+        }
     }
 
     /**
--- resources/views/notifications/list.blade.php
+++ resources/views/notifications/list.blade.php
@@ -4,7 +4,9 @@
     <div>
         <h3>Quản lý thông báo</h3>
         <div style="margin-top: 20px; margin-bottom: 20px;">
-            <a href="#" class="btn btn-xs btn-warning">Create</a>
+            <button type="button" class="btn btn-xs btn-warning" data-toggle="modal" data-target="#createNoti">
+                Create
+            </button>
         </div>
         <table class="table table-bordered" id="notifications-table">
             <thead>
@@ -12,12 +14,51 @@
                 <th>Id</th>
                 <th>Title</th>
                 <th>Content</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                <th>Action</th>
             </tr>
             </thead>
         </table>
     </div>
+
+    <!-- Modal create -->
+    <div class="modal fade" id="createNoti" tabindex="-1" role="dialog" data-target=".bd-example-modal-" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="display: flex; justify-content: center">
+                    <h3 class="modal-title" id="exampleModalLabel">Nội dung thông báo</h3>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('v1.notifications.store') }}" id="create-subject" enctype="multipart/form-data" style="margin: 0px 20px;">
+                        @csrf
+                        <div class="row">
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Tên môn học</label>
+                                    <input id="title" name="title" class="form-control" type="text" placeholder="Nhập vào tiêu đề thông báo ..." required>
+                                    <div style="margin-top: 5px; " id="div_err_title">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Mã môn học</label>
+                                    <textarea rows="25" id="content" name="content" class="form-control" type="text" placeholder="Nhập vào nội dung thông báo ... " required></textarea>
+                                    <div style="margin-top: 5px; " id="div_err_content">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="margin-top: 20px; margin-bottom: 20px; display: flex; justify-content: right; font-size: small;">
+                            <button type="submit" class="btn btn-xs btn-success" style="padding: 8px;">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @stop
 
 @push('scripts')
@@ -36,10 +77,76 @@
                     { data: 'id', name: 'id' },
                     { data: 'title', name: 'title' },
                     { data: 'content', name: 'content' },
-                    { data: 'created_at', name: 'created_at' },
-                    { data: 'updated_at', name: 'updated_at' }
+                    // {
+                    //     data: 'content', render: function (content){
+                    //         return '<p style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2; overflow: hidden; width: 500px;">' + content +'</p>'
+                    //     }
+                    // },
+                    { data: 'action', name: ''},
                 ]
             });
         });
+
+        $("#create-subject").submit(function (e) {
+            e.preventDefault();
+
+            var formData = new FormData();
+
+            formData.append('title', $("#title").val());
+            formData.append('content', $("#content").val());
+
+            $.ajax({
+                url: '{{ route('v1.notifications.store') }}',
+                processData: false,
+                contentType: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('input[name="_token"]').val()
+                },
+                method: "POST",
+                data: formData,
+                success: function (data) {
+                    if (data.response.code === 200) {
+                        toastr.success('Thêm mới thông báo thành công!', 'Success');
+                        window.location = "{{ route('notifications.list') }}";
+                    }
+                },
+                error: function (err) {
+                    if (err.status === 422) {
+                        let errList = err.responseJSON.Err_Message;
+
+                        for (let key in errList) {
+                            $("#div_err_" + key).html(`<p style="color: red; font-size: small;">* ` + errList[key] + `</p>`);
+                        }
+
+                        console.log(err);
+                    }else if (err.status === 500){
+                        toastr.error(err.statusText);
+                        console.log(err);
+                    }
+                },
+            });
+            return true;
+        });
+
+        function deleteNoti(id){
+            if (confirm('Ban co muon xoa khong?') === true) {
+                $.ajax({
+                    url: `http://nina-soft.com/api/v1/notifications/` + id,
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ @csrf_token() }}',
+                        "Authorization": "Bearer " + localStorage.getItem("token"),
+                    },
+                    method: "DELETE",
+                    success: function (data) {
+                        window.location = "{{ route('notifications.list') }}";
+                    },
+                    error: function (err) {
+                        alert('error');
+                        console.log(err);
+                    }
+                });
+            }
+        }
+
     </script>
 @endpush
